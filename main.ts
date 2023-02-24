ff427fd5a2a55a34c31da783606ddab07210bd15
PrithikaM123 <126211128+PrithikaM123@users.noreply.github.com>
2023-02-23 20:32:48 -0800

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,3 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 	
 })
 scene.setBackgroundImage(assets.image`Tittle screen`)
-let mySprite = sprites.create(assets.image`Little boy front`, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)

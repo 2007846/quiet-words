7091e49f35027f060a2ba976d652d659e1da0705
2007846 <93614906+2007846@users.noreply.github.com>
2023-03-18 13:56:05 -0700

    The child stops moving if the buttons aren't pressed, also turned each scene into a function

--- main.ts
+++ main.ts
@@ -1,22 +1,49 @@
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`kid walking left`,
-    100,
-    true
-    )
+    if (Game_State == 1) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`kid walking left`,
+        100,
+        true
+        )
+        Player_Direction = 1
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    if (Game_State == 1) {
+        animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    if (Game_State == 1) {
+        animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`kid walking right`,
-    100,
-    true
-    )
+    if (Game_State == 1) {
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`kid walking right`,
+        100,
+        true
+        )
+        Player_Direction = 0
+    }
 })
+function Title_Screen () {
+    Game_State = 0
+    scene.setBackgroundImage(assets.image`Tittle screen`)
+    pauseUntil(() => controller.A.isPressed())
+    Nav_Screen()
+}
+function Nav_Screen () {
+    Game_State = 1
+    scene.setBackgroundImage(assets.image`myImage`)
+    mySprite = sprites.create(assets.image`Little boy front`, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 100)
+    mySprite.setStayInScreen(true)
+}
+let Player_Direction = 0
 let mySprite: Sprite = null
-scene.setBackgroundImage(assets.image`Tittle screen`)
-pauseUntil(() => controller.A.isPressed())
-scene.setBackgroundImage(assets.image`myImage`)
-mySprite = sprites.create(assets.image`Little boy front`, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
+let Game_State = 0
+Title_Screen()

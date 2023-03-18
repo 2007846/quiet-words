8ad8321453f7bc13a78b2a73f2ab9e54507dc2ea
2007846 <93614906+2007846@users.noreply.github.com>
2023-03-17 18:35:44 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`kid walking left`,
+    100,
+    true
+    )
 })
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`kid walking right`,
+    100,
+    true
+    )
+})
+let mySprite: Sprite = null
 scene.setBackgroundImage(assets.image`Tittle screen`)
+pauseUntil(() => controller.A.isPressed())
+scene.setBackgroundImage(assets.image`myImage`)
+mySprite = sprites.create(assets.image`Little boy front`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
